ed79755bb89f803c7f672421797638b998d8b52a
unknown <jdcollins1004@gmail.com>
2018-04-27 11:08:47 +1000

    easy way to set up environment

---
+++ extras/set up enviroment.bat
@@ -0,0 +1,9 @@
+python -m ensurepip
+python -m pip install --upgrade pip
+pip install kivy
+pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+pip install kivy.deps.gstreamer
+pip install gtts
+pip install requests_html
+pip install pillow
+pause
